db1d5e45ed268ad299f5c266a88648cca6f646b5
ewu-lopezg <glopez22@eagles.ewu.edu>
2014-12-01 18:07:09 -0800

    Populated LoginScreen with Username, Password, and Re-enter text fields as well as labels

--- Trivia Maze One/src/maze/LoginScreen.java
+++ Trivia Maze One/src/maze/LoginScreen.java
@@ -7,10 +7,18 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 
 public class LoginScreen extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField usernameTextField;
+	private JTextField passwordTextField;
+	private JTextField reenterTextField;
 
 	/**
 	 * Launch the application.
@@ -29,11 +37,38 @@ public class LoginScreen extends JDialog {
 	 * Create the dialog.
 	 */
 	public LoginScreen() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 441, 242);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel usernameLabel = new JLabel("Username:");
+		usernameLabel.setBounds(12, 75, 95, 30);
+		contentPanel.add(usernameLabel);
+		
+		JLabel passwordLabel = new JLabel("Password:");
+		passwordLabel.setBounds(12, 106, 71, 16);
+		contentPanel.add(passwordLabel);
+		
+		JLabel reenterLabel = new JLabel("Re-enter:");
+		reenterLabel.setBounds(12, 133, 56, 16);
+		contentPanel.add(reenterLabel);
+		
+		usernameTextField = new JTextField();
+		usernameTextField.setBounds(78, 79, 331, 22);
+		contentPanel.add(usernameTextField);
+		usernameTextField.setColumns(10);
+		
+		passwordTextField = new JTextField();
+		passwordTextField.setBounds(78, 103, 331, 22);
+		contentPanel.add(passwordTextField);
+		passwordTextField.setColumns(10);
+		
+		reenterTextField = new JTextField();
+		reenterTextField.setBounds(78, 130, 331, 22);
+		contentPanel.add(reenterTextField);
+		reenterTextField.setColumns(10);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -46,10 +81,14 @@ public class LoginScreen extends JDialog {
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						LoginScreen.this.dispose();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
 		}
 	}
-
 }
